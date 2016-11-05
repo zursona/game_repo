7e13ca5239e8792d085340294c5b45b85d732a4b
zursona <zurashinjikashvili@gmail.com>
2016-11-06 01:13:35 +0400

    tadaa
    tadaa

---
+++ parts/Assets/Scripts/animatesign.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class animatesign : MonoBehaviour {
+    
+    public Animation animation1;
+    public Animation animationTrain;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            animation1.Play("rotate_right");
+            animationTrain.Play("animate_wheel");
+        }
+        else if (Input.GetMouseButtonDown(1))
+        {
+            animation1.Play("rotate_left");
+            animationTrain.Play("animate_wheel");
+        }
+
+    }
+}
